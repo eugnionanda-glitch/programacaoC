67842339364d276f33f537892f51254631ff96d6
eugnionanda-glitch <eugnionanda@gmail.com>
2026-01-09 19:41:24 +0000

    correção e teste1

--- Exercicio_Calculando_Convertendo.c
+++ Exercicio_Calculando_Convertendo.c
@@ -4,14 +4,7 @@ int main (){
     int nota1;
     int nota2;
     int nota3;
-    int media = nota1 + nota2 + nota3 / 3;
-    const char *status;
-        if (media >=6){
-            status = "Aprovado";
-        } else {
-            status = "Reprovado";
-        }
-
+    
 printf("**MÉDIAS FINAIS**\n");
 printf("Digite a primeira nota do aluno: %d\n", nota1);
 scanf("%d",&nota1);
@@ -22,9 +15,15 @@ scanf("%d",&nota2);
 printf("Digite a terceira nota do aluno: %d\n", nota3);
 scanf("%d",&nota3);
 
+int media = nota1 + nota2 + nota3 / 3;
+    const char *status;
+        if (media >=6){
+            status = "Aprovado";
+        } else {
+            status = "Reprovado";
+        }
+
 printf("A média final do aluno é: %d\n", media);
 printf("O status final do aluno é: %s\n", status);
-
-return 0;
 
 }
