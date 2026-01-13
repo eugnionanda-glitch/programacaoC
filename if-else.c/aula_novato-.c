e37e58ef75c35ae47b9358120ca915a31e67da08
eugnionanda-glitch <eugnionanda@gmail.com>
2026-01-13 16:02:43 +0000

    hr_codar-sem3

---
+++ if-else.c/aula_novato-.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main (){
+    float temperatura, umidade;
+    unsigned int estoque, estoqueMinimo = 1000;
+
+    printf("Entre com a temperatura: \n");
+    scanf("%f", &temperatura);
+
+    printf("Entre com a umidade: \n");
+    scanf("%f",&umidade);
+
+    printf("Entre com o estoque: \n");
+    scanf ("%u", &estoque);
+
+    if (temperatura > 30){
+        printf("A temperatura está alta.\n");
+    }else{ 
+        printf ("A temperatura está dentro do parâmetros.\n");
+    }
+
+    if (umidade > 50){
+        printf ("Umidade elevada.\n");
+    } else{
+        printf ("A umidade está dentro do parâmetros.\n");
+    
+    }
+
+    if (estoque < estoqueMinimo){
+        printf("Estoque abaixo do mínimo.\n");
+    } else{
+        printf ("O estoque está dentro do parâmetros.\n");
+    }
+  
+}
+
