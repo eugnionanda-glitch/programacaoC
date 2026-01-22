faa96e17f5258c9a2b40783691b6b029bf052cdd
eugnionanda-glitch <eugnionanda@gmail.com>
2026-01-22 18:05:48 +0000

    prática while

---
+++ Tema_4.c/codar-.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+
+    int i = 0;
+
+    while (i <=10){
+        if (i % 2 == 0)
+        {
+            printf("O número %d é par!\n", i);
+        } else {
+            printf("O número %d é ímpar!\n", i);
+        }
+        
+        i++;
+    }
+    
+
+    return 0;
+}
