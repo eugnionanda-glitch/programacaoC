cc941773e9803173a551a5637f8a03217fce6b61
eugnionanda-glitch <eugnionanda@gmail.com>
2026-01-22 18:29:12 +0000

    prática for

--- Tema_4.c/codar2-.c
+++ Tema_4.c/codar2-.c
@@ -8,11 +8,10 @@ int main () {
         printf("Digite um número par para sair do programa: \n");
         scanf("%d", &numero);
 
-        if (numero %2 ==0){
+        if (numero %2 ==0) {
             
-        printf("Esse número é par!\n", numero);
-
-        } else {
+        printf ("Esse número é par!\n", numero);
+            } else {
         printf("Esse número é ímpar!\n", numero);
         }
     } while (numero %2 !=0);
