79382b0e68b7ca785c245735723ab170761c23df
eugnionanda-glitch <eugnionanda@gmail.com>
2026-01-31 13:39:42 +0000

    array-aula1

---
+++ Tema_5.c/tm5-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+    int main(){
+        int numero[5] = {10, 20, 30, 40, 50};
+
+        printf("O primeiro número é: %d\n", numero[0]);
+        printf("O último número é: %d\n", numero[4]);
+        return 0;
+
+    }
