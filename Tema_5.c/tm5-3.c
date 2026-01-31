fc9f1581e303f6061c1730ad725609ec2228b039
eugnionanda-glitch <eugnionanda@gmail.com>
2026-01-31 14:22:53 +0000

    matriz-aula1

---
+++ Tema_5.c/tm5-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+    int main(){
+        char letras[2][3] = {
+            {'a', 'b', 'c'},
+            {'d', 'e', 'f'}
+        };
+        
+            printf("%c\n", letras[0][2]);
+            printf("%c\n", letras[1][0]);
+
+    return 0;
+}
+
+
+    
