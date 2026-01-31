01d6b5983a4f0808b88a6ee3d0dce66ca382ffd6
eugnionanda-glitch <eugnionanda@gmail.com>
2026-01-31 13:45:20 +0000

    array-teste2

---
+++ Tema_5.c/tm5-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+    int main(){
+        float notas[3] = {55.7, 68.2, 94.1};
+
+        printf("Notas:\n");
+        printf("A primeira nota é: %.1f\n", notas[0]);
+        printf("A segunda nota é: %.1f\n", notas[1]);
+        printf("A terceira nota é: %.1f\n", notas[2]);
+
+        return 0;
+    }
